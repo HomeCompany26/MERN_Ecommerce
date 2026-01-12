1aa38f580148145087d3249b07f9060daa55a083
rabin <rabinpjpt@gmail.com>
2026-01-12 21:25:43 +0545

    joi validator installed

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -4,6 +4,10 @@ const app = express();
 const router = require("../routes/router");
 const errorRoutes = require("./error.config");
 
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/v1", router);
 app.use(errorRoutes);
 
