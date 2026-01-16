bfe0177341b6333846caec43267e88f223569882
rabin <rabinpjpt@gmail.com>
2026-01-16 16:03:54 +0545

    db connection,env setup

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const dbConnect = require("./db.config");
 
 const router = require("../routes/router");
 const errorRoutes = require("./error.config");
