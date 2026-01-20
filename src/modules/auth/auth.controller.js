761e586b3af8cf5682c44f321303c158fa92f2c4
rabin <rabinpjpt@gmail.com>
2026-01-20 14:25:22 +0545

    bycryptjs used to has password

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,33 +1,16 @@
-const { randomString } = require("../../config/helper.config");
-const EmailService = require("../common/mail/email.service");
-const userModel = require("../../modules/user/user.model");
+const bcrypt = require("bcryptjs");
 const authSvc = require("./auth.service");
 
 class AuthController {
   register = async (req, res) => {
     const payload = req.body;
+    if (req.files) {
+      const images = req.files.map((img) => img.filename);
+      payload.images = images;
+      // console.log(req.files);
+    }
     try {
-      if (req.files) {
-        const images = req.files.map((img) => img.filename);
-        payload.images = images;
-        // console.log(req.files);
-      }
-
-      payload.activationToken = randomString();
-      payload.status = "notactivated";
-      let link = "http://localhost:3000/activate/" + [payload.activationToken];
-      let message = `Dear ${payload.name}, <br/>
-          <p>Your account has been sucessfully registered. please click the link below to activeate your account<p/>
-          <a herf="${link}">${link}</a>
-          <br/>
-          Regards,<br/>
-          System admin<br/>
-          <small>Please do not reply this email.</small>`;
-
-      const emailSvc = new EmailService();
-      console.log(payload);
-      await emailSvc.sendEmail(payload.email, "Activate your account", message);
-      authSvc.register(payload);
+      await authSvc.register(payload);
 
       res.json({ result: "register user", data: payload, meta: null });
     } catch (error) {
@@ -44,13 +27,12 @@ class AuthController {
   activateUser = async (req, res) => {
     const token = req.params.token;
     const user = await authSvc.getUserByActivationToken(token);
+
     const data = {
-      password: req.body.password,
+      password: bcrypt.hashSync(req.body.password, 10),
       activationToken: null,
       status: "activated",
     };
-    console.log(data);
-    console.log(user);
     const response = await authSvc.updateUserById(user._id, data);
     res.json({ result: response, message: "activate userrrr", meta: null });
   };
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -6,9 +6,25 @@ class AuthService {
   //
 
   register = async (payload) => {
-    const user = new userModel(payload);
-    console.log(user);
-    await user.save();
+    try {
+      payload.activationToken = randomString();
+      payload.status = "notactivated";
+      let link = "http://localhost:3000/activate/" + [payload.activationToken];
+      let message = `Dear ${payload.name}, <br/>
+              <p>Your account has been sucessfully registered. please click the link below to activeate your account<p/>
+              <a herf="${link}">${link}</a>
+              <br/>
+              Regards,<br/>
+              System admin<br/>
+              <small>Please do not reply this email.</small>`;
+
+      const emailSvc = new EmailService();
+      await emailSvc.sendEmail(payload.email, "Activate your account", message);
+      const user = new userModel(payload);
+      await user.save();
+    } catch (error) {
+      throw error;
+    }
   };
 
   getUserByActivationToken = async (token) => {
